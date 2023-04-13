f31113384931caa25e972f5e7df72b327f93207a
ann1712 <ann@theparallel.io>
2023-04-13 15:32:56 +0700

    upload metadata

---
+++ contracts/RuneNFT.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract RuneNFT is ERC721, ERC721Enumerable, Ownable {
+
+    // Declare variables;
+    
+    string baseUri = "";
+
+    constructor() ERC721("RuneNFT", "RUNE") {
+    }
+
+    
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721, ERC721Enumerable){
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override (ERC721, ERC721Enumerable) returns (bool){
+        return super.supportsInterface(interfaceId);
+    }
+
+}
